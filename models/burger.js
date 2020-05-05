932722bbb05dff681d05b57c7cceceeca30681a4
Mohmedhusain Vaid <hussainvaid7@gmail.com>
2020-05-04 22:08:29 -0500

    Added comments

--- models/burger.js
+++ models/burger.js
@@ -2,19 +2,23 @@
 const queries = require("../config/orm")
 
 var burger = {
-  // The variables cols and vals are arrays.
+  
+  //call selectAll function from ORM
   getBurgers: async function(){
     let burgers= await queries.selectAll()
     return burgers;
   },
+  //call insertOne function from ORM
   insertBurger: async function(burger){
     var burgers= await queries.insertOne(burger)
     return burgers;
   },
+  //call updateBurger function from ORM
   updateBurger: async function(id){
     var burger = await queries.updateBurger(id)
     return burger;
   },
+  //call getBurgerById function from ORM
   getBurgerById: async function(id){
     var burger = await queries.getBurgerById(id);
     return burger;
@@ -22,5 +26,5 @@ var burger = {
 
 };
 
-// Export the database functions for the controller (catsController.js).
+// Export the database functions for the controller
 module.exports = burger;
